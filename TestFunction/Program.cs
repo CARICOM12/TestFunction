32b80ab0a9a2378e8093d21d86b7015ac39bd540
CARICOM12 <0190a@ukr.net>
2020-05-02 20:19:20 +0300

    Add files via upload

---
+++ TestFunction/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TestFunction
+{
+    class MainClass
+    {
+        public static int triArea(int width, int height)
+        {
+            return (width * height) / 2;
+        }
+        public static int Summ(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+        public static void Main(string[] args)
+        {
+            int x = 33;
+            int y = 22;
+            int z = Summ(x, y);
+            Console.WriteLine(z);
+        }
+    }
+}
